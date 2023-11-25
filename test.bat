54713d6b73f5e397b15a7cffc61e13e835dd7a39
beshi <beshicom@outlook.jp>
2023-11-25 14:28:26 +0900

    テスト用バッチファイルを追加。

---
+++ test.bat
@@ -0,0 +1,33 @@
+@echo off
+
+call :assert 0 0
+call :assert 42 42
+
+echo OK
+
+exit /b 0
+
+:assert
+	setlocal
+
+	set expected=%1
+	set input=%2
+
+	CCompiler %input% > tmp.asm
+	ml /c /coff tmp.asm
+	link /SUBSYSTEM:WINDOWS tmp.obj
+
+	tmp
+	set actual=%errorlevel%
+
+	if %actual% == %expected% (
+		echo %input% ÅÅÅÑ %actual%
+	) else (
+		echo %input% ÅÅÅÑ %expected% expected, but got %actual%
+		exit /b 1
+	)
+
+	echo -----------
+
+	endlocal
+	exit /B 0
